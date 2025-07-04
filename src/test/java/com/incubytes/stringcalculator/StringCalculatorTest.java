16e81696d5119c0bb7f5d2df0a0fac4aa268edee
vatsalk7 <vatsal3616@gmail.com>
2025-07-05 00:51:17 +0530

    Object Creation

--- src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
@@ -1,8 +1,15 @@
 package com.incubytes.stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
 
     @Test
     public void empty(){
