aaf5f372a0df9c0fe88ba798dd688340f5220f40
vatsalk7 <vatsal3616@gmail.com>
2025-07-05 00:32:05 +0530

    Initial commit: Basic test to confirm test suite runs✅

--- src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
@@ -1,4 +1,11 @@
 package com.incubytes.stringcalculator;
 
+import org.junit.jupiter.api.Test;
+
 public class StringCalculatorTest {
+
+    @Test
+    public void empty(){
+        // Test with the most basic case to confirm the program runs correctly.
+    }
 }
