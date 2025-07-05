6b7b4b59821744e0528d66c16ed2bcdac9247f21
vatsalk7 <vatsal3616@gmail.com>
2025-07-05 17:23:01 +0530

    Test 6: Support custom delimiter format //;\n1;2✅

--- src/main/java/com/incubytes/stringcalculator/StringCalculator.java
+++ src/main/java/com/incubytes/stringcalculator/StringCalculator.java
@@ -4,7 +4,15 @@ public class StringCalculator {
 
         public int add(String numbers) {
             if (numbers.isEmpty()) return 0;
-            String[] tokens = numbers.split("[,\n]");
+
+            String delimiter = ",|\n";
+            if (numbers.startsWith("//")) {
+                int delimiterIndex = numbers.indexOf("\n");
+                delimiter = numbers.substring(2, delimiterIndex);
+                numbers = numbers.substring(delimiterIndex + 1);
+            }
+
+            String[] tokens = numbers.split(delimiter);
             int sum = 0;
             for (String num : tokens) {
                 sum += Integer.parseInt(num);
--- src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/incubytes/stringcalculator/StringCalculatorTest.java
@@ -46,5 +46,12 @@ public class StringCalculatorTest {
         assertEquals(6, calculator.add("1\n2,3"));
     }
 
+    @Test
+    void shouldSupportCustomDelimiter() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+
 
 }
